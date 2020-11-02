d8b71589cac1c5c4dbb6abf3d1152537d5353dc0
Chayejin0428 <chayezin@gmail.com>
2020-11-02 16:59:08 +0900

    레이아웃 수정(내용 넣기 전)

---
+++ cap012/app/src/main/java/com/example/cap01/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.cap01;
+
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
